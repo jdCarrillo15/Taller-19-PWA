df7197c49a60422e78e29b96a82a47fceff657d7
jdCarrillo15 <juandcarrillop15@gmail.com>
2026-05-08 22:01:49 -0500

    app funcionando en local host, hagan sus ramas :P

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
-const app = require('./src/app');
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+ res.send('Taller-19-PWA funcionando');
+});
+
 app.listen(port, () => {
- console.log(`Servidor corriendo en puerto ${port}`);
+ console.log(`Servidor en [http://localhost:${port}\`](http://localhost:${port}`);
 });
